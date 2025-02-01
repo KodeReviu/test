bd5e127c250f0f08cdb38ff0adcfd6ea1ace273b
NoNeName19 <nonenamenik@gmail.com>
2025-02-01 11:53:44 +0700

    Create calc_plus.cs

---
+++ calc_plus.cs
@@ -0,0 +1,6 @@
+// Сложение
+Console.WriteLine("Введите 1-ое число");
+int number_one = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число");
+int number_two = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number_one + "+" + number_two + "=" + number_one + number_two);
